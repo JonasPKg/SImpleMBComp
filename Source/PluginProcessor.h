33dbd02e6de07bc7124867b1ab993c0a59e95890
JonasPKg <85122949+JonasPKg@users.noreply.github.com>
2024-06-24 14:05:00 +0200

    Added IO Gain, cleaned up DSP

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -17,7 +17,7 @@ Roadmap
 4) create audio parameterse for the 3 compresso bands and need to live on each band instance DONE
 5) add 2 remaining compressors DONE
 6) add ability to mute/solo/bypass individual compressors DONE
-7) add input and output gain to offset changes in output level
+7) add input and output gain to offset changes in output level DONE
 8) clean up anything that needs cleaning up
 */
 
@@ -57,6 +57,9 @@ enum Names
     Solo_Low_Band,
     Solo_Mid_Band,
     Solo_High_Band,
+
+    Gain_In,
+    Gain_Out,
 };
 
 inline const std::map<Names, juce::String>& GetParams() 
@@ -90,6 +93,8 @@ inline const std::map<Names, juce::String>& GetParams()
         {Solo_Mid_Band, "Solo Mid Band"},
         {Solo_High_Band, "Solo High Band"},
 
+        {Gain_In, "Gain In"},
+        {Gain_Out, "Gain Out"}
     };
 
     return params;
@@ -200,6 +205,22 @@ private:
     juce::AudioParameterFloat* midHighCrossover{ nullptr };
 
     std::array<juce::AudioBuffer<float>, 3> filterBuffers;
+
+    juce::dsp::Gain<float> inputGain, outputGain;
+    juce::AudioParameterFloat* inputGainParam{ nullptr };
+    juce::AudioParameterFloat* outputGainParam{ nullptr };
+
+    template<typename T, typename U>
+    void applyGain(T& buffer, U& gain)
+    {
+        auto block = juce::dsp::AudioBlock<float>(buffer);
+        auto ctx = juce::dsp::ProcessContextReplacing<float>(block);
+        gain.process(ctx);
+    }
+
+    void updateState();
+
+    void splitBands(const juce::AudioBuffer<float>& inputBuffer);
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleMBCompAudioProcessor)
 };
